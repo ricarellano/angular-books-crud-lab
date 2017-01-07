205ce9a2bc7ceabdd51b1644c39964ef8d3f40bb
ricarellano <rarellanob@gmail.com>
2017-01-06 21:43:15 -0800

    working show controller

--- controllers/BooksIndexController.js
+++ controllers/BooksIndexController.js
@@ -3,3 +3,22 @@ angular.module('libraryApp')
 
 // add your BooksIndexController function here!
 // don't forget $http if you need to make requests
+
+BooksIndexController.$inject = ['$http'];
+function BooksIndexController($http){
+  var vm = this;
+
+  $http({
+    method: 'GET',
+    url: 'https://super-crud.herokuapp.com/books'
+  }).then(function successCallback(response){
+    vm.books = response.data.books;
+    console.log(response.data.books);
+  }, function errorCallback(response){
+    console.log('There was an error getting the data', response);
+
+  });
+
+
+
+};
--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -2,9 +2,24 @@ angular.module('libraryApp')
   .controller('BooksShowController', BooksShowController);
 
 // controller function and dependency injection
-// $routeParams and $location are required for routing stuff 
+// $routeParams and $location are required for routing stuff
 //   - but you might need to add a dependency
-BooksShowController.$inject=['$routeParams', '$location'];   
-function BooksShowController($routeParams, $location) {
+
+BooksShowController.$inject=['$http','$routeParams', '$location'];
+function BooksShowController($http, $routeParams, $location) {
   var vm = this;
+  var bookId = $routeParams.id;
+
+
+  $http({
+    method: 'GET',
+    url: 'https://super-crud.herokuapp.com/books/' + bookId,
+  }).then(function succesCallback(response){
+    vm.book = response.data;
+    console.log(response.data);
+  }, function errorCallback(response){
+    console.log('There was an error getting the data', response);
+
+  });
+
 };
