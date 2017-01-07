570cfbee4bd6de0b84cbaa8d23738189885fffb6
ricarellano <rarellanob@gmail.com>
2017-01-07 14:48:42 -0800

    working update and delete

--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -22,4 +22,32 @@ function BooksShowController($http, $routeParams, $location) {
 
   });
 
+vm.updateBook = function (book){
+  $http({
+    method:'PUT',
+    url: 'https://super-crud.herokuapp.com/books/' + bookId,
+    data: {
+      title: vm.book.title,
+      author: vm.book.author,
+      image: vm.book.image,
+      releaseDate: vm.book.releaseDate
+    }
+  }).then(function successCallback(updateBook){
+     console.log(updateBook);
+  }, function errorCallback(response){
+     console.log('There was an error getting the data', response);
+});
+}
+
+vm.deleteBook = function(book){
+  $http({
+    method: 'DELETE',
+    url: 'https://super-crud.herokuapp.com/books/' + bookId,
+  }).then(function successCallback(deleteBook){
+    console.log(deleteBook);
+  },function errorCallback(response){
+    console.log('There was an error getting the data', response);
+  });
+}
+
 };
